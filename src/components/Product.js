ce4cf8c3b5db963b8e5d2fc8e04c1fb6fb60a03c
Willy Huang <willyhuang18@gmail.com>
2022-10-14 16:06:05 -0700

    make the product section responsive

--- src/components/Product.js
+++ src/components/Product.js
@@ -14,7 +14,9 @@ function Product({ id, title, price, description, category, image }) {
 
   return (
     <div className="relative flex flex-col m-5 bg-white z-30 p-10">
-      <p className="absolute top-2 right-2 text-sm italic text-gray-400">{category}</p>
+      <p className="absolute top-2 right-2 text-sm italic text-gray-400">
+        {category}
+      </p>
 
       <Image src={image} width={200} height={200} objectFit="contain" />
       <h4>{title}</h4>
