77538238eaee18a25e47cf4cd59a4536f4ad06b6
Willy Huang <willyhuang18@gmail.com>
2022-10-13 21:53:47 -0700

    Finishing the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ import {
 function Header() {
   return (
     <header>
-      <div className="flex item-center bg-amazon_blue-light p-1 flex-grow py-2 ">
+      <div className="flex item-center p-1 flex-grow py-2 bg-amazon_blue ">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
             src="https://links.papareact.com/f90"
@@ -44,15 +44,35 @@ function Header() {
             <p className="font-extrabold md:text-sm">& Order</p>
           </div>
           <div className="relative link flex items-center">
-            <span className="absolute top-0 right-0 md:right-5 h-4 w-4 bg-yellow-400
-            text-center rounded-full text-black font-bold">4</span>
+            <span
+              className="absolute top-0 right-0 md:right-5 h-4 w-4 bg-yellow-400
+            text-center rounded-full text-black font-bold"
+            >
+              4
+            </span>
             <ShoppingCartIcon className="h-10 " />
-            <p className="font-extrabold md:text-sm md:inline hidden mt-2">Cart</p>
+            <p className="font-extrabold md:text-sm md:inline hidden mt-2">
+              Cart
+            </p>
           </div>
         </div>
       </div>
-
-      <div></div>
+      {/* Bottom of the Nav bar */}
+      <div className="flex items-center space-x-3 p-2 pl-6  bg-amazon_blue-light text-white">
+        <p className="link flex items-center">
+          <MenuIcon className="h-6 mr-1" />
+          All
+        </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Today's Deal</p>
+        <p className="link hidden lg:inline-flex">Electronics</p>
+        <p className="link hidden lg:inline-flex">Food & rocery</p>
+        <p className="link hidden lg:inline-flex">Prime</p>
+        <p className="link hidden lg:inline-flex">Buy Again</p>
+        <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+        <p className="link hidden lg:inline-flex">Health & Personal Care</p>
+      </div>
     </header>
   );
 }
