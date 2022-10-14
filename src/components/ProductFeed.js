170341613cc7e310b75adf09570ea9cb4209207a
Willy Huang <willyhuang18@gmail.com>
2022-10-14 15:37:26 -0700

    passing the product component

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from "react";
+import Product from "./Product";
 
-function ProductFeed({products}) {
+function ProductFeed({ products }) {
   return (
     <div>
-        {products.map(product =>(
-            <p>{product.title}</p>
-        ))}
+      {products.map(({ id, title, price, description, category, image }) => (
+        <Product
+          key={id}
+          id={id}
+          title={title}
+          price={price}
+          description={description}
+          category={category}
+          image={image}
+        />
+      ))}
     </div>
-  )
+  );
 }
 
-export default ProductFeed
+export default ProductFeed;
