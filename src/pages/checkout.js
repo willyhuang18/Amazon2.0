4e5b4b8792743069af9f9d75b00fb826e0615e70
Willy Huang <willyhuang18@gmail.com>
2022-10-15 22:22:38 -0700

    Building the Left Side of Checkout Page

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,24 +1,30 @@
 import React from "react";
-import Header from '../components/Header';
+import Header from "../components/Header";
 import Image from "next/image";
 
 function Checkout() {
   return (
     <div className="bg-gray-100">
-        <Header />
+      <Header />
 
-        <main className="lg:flex max-w-screen-2xl mx-auto">
-          {/* left */}
-          <div>
-            <Image src="https://links.papareact.com/ikj"
+      <main className="lg:flex max-w-screen-2xl mx-auto">
+        {/* left */}
+        <div className="flex-grow m-5 shadow-sm">
+          <Image
+            src="https://links.papareact.com/ikj"
             width={1020}
             height={250}
-            objectFit="contain"/>
+            objectFit="contain"
+          />
+          <div className="flex flex-col p-5 space-y-10 bg-white">
+            <h1 className=" text-3xl border-b pb-4 ">
+              Your Shopping Cart is Empty
+            </h1>
           </div>
-
-          {/* right */}
-          <div></div>
-        </main>
+        </div>
+        {/* right */}
+        <div></div>
+      </main>
     </div>
   );
 }
