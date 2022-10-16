90717ff7873d69facd3f3d48894ddbfb0e4c2f89
Willy Huang <willyhuang18@gmail.com>
2022-10-16 10:28:39 -0700

    Building the Right Side of Checkout Page

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -4,9 +4,11 @@ import Image from "next/image";
 import { useSelector } from "react-redux";
 import { selectItems } from "../slices/basketSlice";
 import CheckoutProduct from "../components/CheckoutProduct";
+import { useSession } from "next-auth/react";
 
 function Checkout() {
   const items = useSelector(selectItems);
+  const { data: session } = useSession();
   return (
     <div className="bg-gray-100">
       <Header />
@@ -42,7 +44,26 @@ function Checkout() {
           </div>
         </div>
         {/* right */}
-        <div></div>
+        <div className="flex flex-col bg-white p-10 shadow-md">
+          {items.length > 0 && (
+            <>
+              <h2 className=" whitespace-nowrap">
+                Subtotal ({items.length} items) :
+                <span className=" font-bold"></span>
+              </h2>
+              <button
+                disabled={!session}
+                className={`button mt-2
+              ${
+                !session &&
+                "from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed "
+              }`}
+              >
+                {!session ? "Sign in to checkout" : "Proceed to Checkout"}
+              </button>
+            </>
+          )}
+        </div>
       </main>
     </div>
   );
